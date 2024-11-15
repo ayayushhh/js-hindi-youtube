78c1d14f50b33d4dabdf18f7a7334dbae6e7ae7e
Ayush Thakur <131467431+ayayushhh@users.noreply.github.com>
2024-11-15 04:51:29 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+//alert("Hello") //we are using nodejs not browser
+
+console.log(3+3);
+
+//code readability should be high
+
+console.log("Hi, Ayush.");
+
+
+let name = "Ayush"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null);   //null is object
+console.log(typeof undefined);  //undefined is undefined
