0508e3ee0e5bb5ee74df0085d39c626773a1c7b1
Ayush Thakur <131467431+ayayushhh@users.noreply.github.com>
2024-11-22 10:15:50 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,41 @@
+// array
+
+const myArray = [0, 1, 2, 3, 4, 6];
+const myHeroes = ["ayush", "ayush2"];
+/*
+console.log(myArray[5]);
+console.log(myHeroes.length);
+*/
+
+// array methods
+/*
+myArray.push(43);
+myArray.pop();
+console.log(myArray);
+*/
+
+/*
+myArray.unshift(9);
+myArray.shift();
+*/
+/*
+console.log(myArray.includes(9));
+console.log(myArray.indexOf(3));
+
+const newArr = myArray.join()
+
+console.log(myArray);
+console.log(typeof newArr);
+*/
+
+console.log("A", myArray);
+
+const myn1 = myArray.slice(1,3);
+
+console.log(myn1);
+console.log("B", myArray);
+
+
+const myn2 = myArray.splice(1,3);
+console.log("C", myArray);
+console.log(myn2);
